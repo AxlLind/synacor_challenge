dd0ff7c731b03bc5fb4cf191488cfc69669368db
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-02-13 18:41:21 +0800

    Make solve_coins into separate binary

---
+++ src/bin/solve_coins.rs
@@ -0,0 +1,26 @@
+use itertools::Itertools;
+
+/*
+  e are given coins with values 2,3,5,7,9
+  and this equation to solve:
+    _ + _ * _^2 + _^3 - _ = 399
+  only 5! = 120 combinations to check so
+  lets just brute force a solution
+*/
+
+fn main() {
+  let answer = [2,3,5,7,9].iter()
+    .permutations(5)
+    .find(|p| p[0] + p[1]*p[2]*p[2] + p[3]*p[3]*p[3] - p[4] == 399)
+    .unwrap();
+  for i in answer {
+    match i {
+      2 => println!("use red coin"),
+      3 => println!("use corroded coin"),
+      5 => println!("use shiny coin"),
+      7 => println!("use concave coin"),
+      9 => println!("use blue coin"),
+      _ => panic!(),
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
 use std::fs;
+use std::io::Result;
 use itertools::Itertools;
 use easy_io::InputReader;
 
 mod cpu;
 use cpu::{CPU, ExitCode};
 
-mod solve_coins;
-use solve_coins::brute_force_coins;
+const INPUT_FILES: [&str; 3] = [
+  "./inputs/fetch_coins.txt",
+  "./inputs/coin_order.txt",
+  "./inputs/after_coins.txt",
+];
 
-fn read_program(path: &str) -> Vec<u16> {
-  fs::read(path).unwrap()
-    .iter()
+fn read_program(path: &str) -> Result<Vec<u16>> {
+  let buf = fs::read(path)?;
+  let program = buf.iter()
     .tuples()
     .map(|(&a,&b)| (b as u16) << 8 | a as u16)
+    .collect();
+  Ok(program)
+}
+
+fn fetch_inputs() -> Result<Vec<String>> {
+  INPUT_FILES.iter()
+    .map(fs::read_to_string)
     .collect()
 }
 
-fn fetch_inputs() -> Vec<String> {
-  let fetch_coins = fs::read_to_string("./inputs/fetch_coins.txt").unwrap();
-  let after_coins = fs::read_to_string("./inputs/after_coins.txt").unwrap();
-  let coin_order  = brute_force_coins().join("\n") + "\n";
-  vec![fetch_coins, coin_order, after_coins]
-}
-
-fn main() {
-  let program = read_program("./challenge.bin");
+fn main() -> Result<()> {
+  let program = read_program("./challenge.bin")?;
   let mut cpu = CPU::new(&program);
   let mut input = InputReader::new();
 
-  for s in &fetch_inputs() { cpu.push_str(s); }
+  for s in &fetch_inputs()? { cpu.push_str(s); }
   loop {
     match cpu.execute() {
       ExitCode::NeedInput => cpu.push_str(&input.next_line()),
       ExitCode::Halted    => break,
     }
   }
+  Ok(())
 }
