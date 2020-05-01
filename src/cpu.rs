7cc054984cf40a2e466428e807b8e6aea97b03eb
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-01 15:16:47 +0200

    Cpu: Clean up implementation

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,25 +1,22 @@
 use std::collections::VecDeque;
 
-const HLT: u16 = 0;  const SET: u16 = 1;
-const PSH: u16 = 2;  const POP: u16 = 3;
-const EQ:  u16 = 4;  const GT:  u16 = 5;
-const JMP: u16 = 6;  const JT:  u16 = 7;
-const JF:  u16 = 8;  const ADD: u16 = 9;
-const MUL: u16 = 10; const MOD: u16 = 11;
-const AND: u16 = 12; const OR:  u16 = 13;
-const NOT: u16 = 14; const RD:  u16 = 15;
-const WRT: u16 = 16; const CLL: u16 = 17;
-const RET: u16 = 18; const OUT: u16 = 19;
-const IN:  u16 = 20; const NOP: u16 = 21;
+const HLT: u16 = 0;  const SET: u16 = 1;  const PSH: u16 = 2;
+const POP: u16 = 3;  const EQ:  u16 = 4;  const GT:  u16 = 5;
+const JMP: u16 = 6;  const JT:  u16 = 7;  const JF:  u16 = 8;
+const ADD: u16 = 9;  const MUL: u16 = 10; const MOD: u16 = 11;
+const AND: u16 = 12; const OR:  u16 = 13; const NOT: u16 = 14;
+const RD:  u16 = 15; const WRT: u16 = 16; const CLL: u16 = 17;
+const RET: u16 = 18; const OUT: u16 = 19; const IN:  u16 = 20;
+const INST_LEN: [u16;22] = [0,3,2,2,4,4,0,3,3,4,4,4,4,4,3,3,3,2,0,2,0,1];
 
 pub enum ExitCode { Output(u16), NeedInput, Halted }
 
 pub struct CPU {
   pub reg: [u16; 8],
   mem: [u16; 0x8000],
-  stack: VecDeque<u16>,
+  stack: Vec<u16>,
   input: VecDeque<u16>,
-  pc: usize,
+  pc: u16,
 }
 
 impl CPU {
@@ -29,7 +26,7 @@ impl CPU {
     Self {
       reg: [0; 8],
       mem,
-      stack: VecDeque::new(),
+      stack: Vec::new(),
       input: VecDeque::new(),
       pc: 0,
     }
@@ -38,24 +35,25 @@ impl CPU {
   pub fn execute(&mut self) -> ExitCode {
     loop {
       let (op,a,b,c) = self.fetch_args();
-      self.incr_pc(op);
+      self.pc += INST_LEN[op as usize];
       match op {
-        JMP => self.pc = a,
         SET => self.reg[a] = b,
-        EQ  => self.reg[a] = (b == c) as u16,
-        GT  => self.reg[a] = (b > c)  as u16,
+        OR  => self.reg[a] = b | c,
+        AND => self.reg[a] = b & c,
+        MOD => self.reg[a] = b % c,
+        NOT => self.reg[a] = !b & 0x7FFF,
         ADD => self.reg[a] = (b + c) & 0x7FFF,
         MUL => self.reg[a] = (b * c) & 0x7FFF,
-        NOT => self.reg[a] = !b & 0x7FFF,
-        MOD => self.reg[a] = b % c,
-        AND => self.reg[a] = b & c,
-        OR  => self.reg[a] = b | c,
+        GT  => self.reg[a] = (b > c) as u16,
+        EQ  => self.reg[a] = (b == c) as u16,
+        POP => self.reg[a] = self.stack.pop().unwrap(),
         RD  => self.reg[a] = self.mem[b as usize],
         WRT => self.mem[a] = b,
-        PSH => self.stack.push_back(a as u16),
-        POP => self.reg[a] = self.stack.pop_back().expect("stack underflow"),
-        JT  => if a != 0 { self.pc = b as usize },
-        JF  => if a == 0 { self.pc = b as usize },
+        PSH => self.stack.push(a as u16),
+        RET => self.pc = self.stack.pop().unwrap(),
+        JMP => self.pc = a as u16,
+        JT  => if a != 0 { self.pc = b },
+        JF  => if a == 0 { self.pc = b },
         HLT => return ExitCode::Halted,
         OUT => return ExitCode::Output(a as u16),
         IN  => match self.input.pop_front() {
@@ -65,59 +63,31 @@ impl CPU {
           }
           None => return ExitCode::NeedInput,
         },
-        RET => match self.stack.pop_back() {
-          Some(a) => self.pc = a as usize,
-          None    => return ExitCode::Halted,
-        },
         CLL => {
-          self.stack.push_back(self.pc as u16);
-          self.pc = a;
+          self.stack.push(self.pc);
+          self.pc = a as u16;
         },
-        NOP => {},
-        _   => panic!("invalid opcode {}", op)
+        _ => {},
       }
     }
   }
 
   pub fn push_str(&mut self, s: &str) {
-    for b in s.bytes() { self.push_input(b); }
-    self.push_input(b'\n');
+    self.input.extend(s.bytes().map(|b| b as u16));
+    self.input.push_back(b'\n' as u16);
   }
-}
 
-// private methods
-impl CPU {
   fn fetch_args(&self) -> (u16,usize,u16,u16) {
-    let op = self.mem[self.pc];
+    let op = self.mem[self.pc as usize];
     let a = match op {
       PSH|JMP|JT|JF|CLL|WRT|OUT => self.read_adr(1),
-      _ => self.mem[self.pc+1] - 0x8000,
+      _ => self.mem[self.pc as usize + 1] - 0x8000,
     };
-    let b = self.read_adr(2);
-    let c = self.read_adr(3);
-    (op, a as usize, b, c)
-  }
-
-  fn incr_pc(&mut self, op: u16) {
-    self.pc += match op {
-      EQ|GT|ADD|MUL|MOD|AND|OR => 4,
-      SET|JT|JF|NOT|RD|WRT     => 3,
-      PSH|POP|CLL|OUT          => 2,
-      NOP                      => 1,
-      _                        => 0,
-    };
+    (op, a as usize, self.read_adr(2), self.read_adr(3))
   }
 
   fn read_adr(&self, offset: usize) -> u16 {
-    let v = self.mem[self.pc + offset];
-    match v {
-      0x0000..=0x7FFF => v,
-      0x8000..=0x8007 => self.reg[(v - 0x8000) as usize],
-      _               => unreachable!()
-    }
-  }
-
-  fn push_input<T: Into<u16>>(&mut self, t: T) {
-    self.input.push_back(t.into());
+    let v = self.mem[self.pc as usize + offset];
+    *self.reg.get((v - 0x8000) as usize).unwrap_or(&v)
   }
 }
